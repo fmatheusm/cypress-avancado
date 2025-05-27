f49afbd5c4f8642f0c3aa9b25c3b70636a992913
Matheus Martins <matheuss_fernandes@outlook.com>
2025-05-27 12:20:51 -0300

    .

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -52,69 +52,83 @@ describe("Hacker Stories", () => {
   });
 
   context("Mocking the api", () => {
-    beforeEach(() => {
-      cy.intercept("GET", `**/search?query=${initialTerm}&page=0`, {
-        fixture: "stories",
-      }).as("getStories");
+    context("Footer and list of stories", () => {
+      beforeEach(() => {
+        cy.intercept("GET", `**/search?query=${initialTerm}&page=0`, {
+          fixture: "stories",
+        }).as("getStories");
 
-      cy.visit("/");
-      cy.wait("@getStories");
-    });
-
-    it("shows the footer", () => {
-      cy.get("footer")
-        .should("be.visible")
-        .and("contain", "Icons made by Freepik from www.flaticon.com");
-    });
-
-    context("List of stories", () => {
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I assert on the data?
-      // This is why this test is being skipped.
-      // TODO: Find a way to test it out.
-      it.skip("shows the right data for all rendered stories", () => {});
-
-      it.only("shows only less story after dimissing the first one", () => {
-        cy.get(".button-small").first().click();
-
-        cy.get(".item").should("have.length", 1);
+        cy.visit("/");
+        cy.wait("@getStories");
       });
 
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I test ordering?
-      // This is why these tests are being skipped.
-      // TODO: Find a way to test them out.
-      context.skip("Order by", () => {
-        it("orders by title", () => {});
-
-        it("orders by author", () => {});
-
-        it("orders by comments", () => {});
-
-        it("orders by points", () => {});
+      it("shows the footer", () => {
+        cy.get("footer")
+          .should("be.visible")
+          .and("contain", "Icons made by Freepik from www.flaticon.com");
       });
 
-      // Hrm, how would I simulate such errors?
-      // Since I still don't know, the tests are being skipped.
-      // TODO: Find a way to test them out.
-      context.skip("Errors", () => {
-        it('shows "Something went wrong ..." in case of a server error', () => {});
+      context("List of stories", () => {
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I assert on the data?
+        // This is why this test is being skipped.
+        // TODO: Find a way to test it out.
+        it.skip("shows the right data for all rendered stories", () => {});
 
-        it('shows "Something went wrong ..." in case of a network error', () => {});
+        it("shows only less story after dimissing the first one", () => {
+          cy.get(".button-small").first().click();
+
+          cy.get(".item").should("have.length", 1);
+        });
+
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I test ordering?
+        // This is why these tests are being skipped.
+        // TODO: Find a way to test them out.
+        context.skip("Order by", () => {
+          it("orders by title", () => {});
+
+          it("orders by author", () => {});
+
+          it("orders by comments", () => {});
+
+          it("orders by points", () => {});
+        });
       });
     });
 
     context("Search", () => {
       beforeEach(() => {
-        cy.intercept({
-          method: "GET",
-          pathname: "**/search",
-          query: {
-            query: newTerm,
+        cy.intercept(
+          {
+            method: "GET",
+            pathname: "**/search",
+            query: {
+              query: initialTerm,
+            },
           },
-        }).as("getNewTermStories");
+          {
+            fixture: "empty",
+          }
+        ).as("getEmptyStories");
+
+        cy.intercept(
+          {
+            method: "GET",
+            pathname: "**/search",
+            query: {
+              query: newTerm,
+            },
+          },
+          {
+            fixture: "stories",
+          }
+        ).as("getStories");
+
+        cy.visit("/");
+        cy.wait("@getEmptyStories");
 
         cy.get("#search").clear();
       });
@@ -122,10 +136,9 @@ describe("Hacker Stories", () => {
       it("types and hits ENTER", () => {
         cy.get("#search").type(`${newTerm}{enter}`);
 
-        cy.wait("@getNewTermStories");
+        cy.wait("@getStories");
 
-        cy.get(".item").should("have.length", 20);
-        cy.get(".item").first().should("contain", newTerm);
+        cy.get(".item").should("have.length", 2);
         cy.get(`button:contains(${initialTerm})`).should("be.visible");
       });
 
